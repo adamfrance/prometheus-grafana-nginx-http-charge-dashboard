21d8f54ae64aa78487531472d31611630404aca4
M C <mc@Mac-mini-de-M.lan>
2025-04-16 15:29:09 +0200

    first commit

---
+++ k6/http-status-test.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '1m', target: 50 },  // Montée à 50 users
+    { duration: '2m', target: 50 },  // Maintien 50 users
+    { duration: '30s', target: 0 },  // Descente
+  ],
+};
+
+export default function () {
+  http.get('http://webapp/');
+  http.get('http://webapp/nonexistent');
+  sleep(1);
+}
